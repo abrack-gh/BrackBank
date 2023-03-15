a10537c93711fe831a745009b0bb2634bc580190
Alex Brackenridge <a.brack@live.co.uk>
2023-03-15 19:00:13 +0000

    Java modified. Controller for Login created. App.java updated.

---
+++ src/main/java/com/example/brackbank/Controllers/Client/ClientMenuController.java
@@ -0,0 +1,4 @@
+package com.example.brackbank.Controllers.Client;
+
+public class ClientMenuController {
+}
--- src/main/java/com/example/brackbank/Controllers/LoginController.java
+++ src/main/java/com/example/brackbank/Controllers/LoginController.java
@@ -1,4 +1,18 @@
 package com.example.brackbank.Controllers;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class LoginController{
+
+    public ChoiceBox acc_selector;
+    public Label payee_address_lbl;
+    public TextField payee_address_field;
+    public TextField pw_field;
+    public Button login_btn;
+    public Label error_lbl;
+
+
 }
